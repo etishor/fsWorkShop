2a3c3d8b954702e51e761705aa247c91c62e543f
Iulian Margarintescu <etishor@gmail.com>
2014-11-23 11:44:50 +0200

    use array.parallel

--- Beginning.fsx
+++ Beginning.fsx
@@ -2,28 +2,23 @@
 open System
 open System.IO
 
-let skipHeader (a:string[]) = a.[ 1 .. a.Length-1 ]
-let splitCSVString (s:string) = s.Split ','
-
-let convertToInt (s:string) = int s
-let convertToLineOfInts s = Array.map convertToInt s
-
 type BitmapData = { Number:int; Pixels: int[] }
 
-let convertToBitmap (data : int[])  = { Number = data.[0]; Pixels = data.[ 1 .. data.Length-1 ] }  
+let readFile path = File.ReadAllLines path
+let skipHeader (a:string[]) = a.[1..]
+let splitCSVString (s:string) = s.Split ','
+let mapToInteger line = Array.map (fun s -> int s) line
+let convertToBitmap (data : int[])  = { Number = data.[0]; Pixels = data.[1..] }  
 
 let readCSVFile path =
-    File.ReadAllLines path
+    __SOURCE_DIRECTORY__ + Path.DirectorySeparatorChar.ToString() + path 
+    |> readFile
     |> skipHeader 
-    |> Array.map splitCSVString
-    |> Array.map convertToLineOfInts
-    |> Array.map convertToBitmap
+    |> Array.Parallel.map splitCSVString
+    |> Array.Parallel.map mapToInteger
+    |> Array.Parallel.map convertToBitmap
 
-let trainingData = readCSVFile @"/Users/iulianmargarintescu/Projects/Digits/trainingsample.csv"
-
-let matchPoints p1 p2 = (p1,p2)
-
-let pow2difference p1 p2 = (p1 - p2) * (p1 - p2)
+let pow2difference p1 p2 = pown (p1 - p2) 2
 
 let euclideanDistance (first : int[]) (second : int[]) =
     Array.map2 pow2difference first second 
@@ -33,25 +28,20 @@ let euclideanDistance (first : int[]) (second : int[]) =
 
 let distanceFrom (data : BitmapData) (target : BitmapData)  = 
     euclideanDistance target.Pixels data.Pixels
+
+
+let trainingData = readCSVFile "trainingsample.csv"
+let validationData = readCSVFile "validationsample.csv"
  
 let clasify (bitmap: BitmapData) =
-    let distance = distanceFrom bitmap
-
-    let found = 
-        trainingData
-        |> Array.minBy distance
-
-    found.Number
-
-
-let validationData = readCSVFile @"/Users/iulianmargarintescu/Projects/Digits/validationsample.csv"
+    trainingData  
+    |> Array.minBy (distanceFrom bitmap)
 
 let compare (data : BitmapData) =
     let result = clasify data
-    if data.Number = result then 1. else 0.
+    if data.Number = result.Number then 1. else 0.
  
-let matches = 
+let result = 
     validationData 
-    |> Array.map compare
+    |> Array.Parallel.map compare
     |> Array.average
-
